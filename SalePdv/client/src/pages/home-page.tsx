ef9e38c422f174b10fa5c4b4849307cd58572f3f
username11119 <16262921-username11119@users.noreply.replit.com>
2025-04-22 17:23:06 +0000

    Restored to 'e402b005bc2673ad61091dfe017bb41abc2501c4'
    Replit-Restored-To: e402b005bc2673ad61091dfe017bb41abc2501c4

--- SalePdv/client/src/pages/home-page.tsx
+++ SalePdv/client/src/pages/home-page.tsx
@@ -1,13 +1,19 @@
+
 import { useEffect } from "react";
 import { useLocation } from "wouter";
+import { useAuth } from "@/hooks/use-auth";
 
 export default function HomePage() {
   const [, navigate] = useLocation();
+  const { user } = useAuth();
   
-  // Redirect to dashboard
   useEffect(() => {
-    navigate("/dashboard");
-  }, [navigate]);
+    if (user) {
+      navigate(user.role === "admin" ? "/admin" : "/order");
+    } else {
+      navigate("/auth");
+    }
+  }, [user, navigate]);
   
   return null;
 }
